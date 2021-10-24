2eee3b026f45dd0c6bf0ed1acc201c4e40215b74
Manuel Monteiro Dev <m@manuelmonteiro.dev>
2021-10-24 13:23:42 +0200

    Update preload attribute enqueue.php

--- inc/functions/enqueue.php
+++ inc/functions/enqueue.php
@@ -151,6 +151,23 @@ add_action( 'enqueue_block_editor_assets', 'rimozione_gutenberg_fullscreen' );
 
 
 
+/**
+ * Inserisco il preload nel link rel [Google page speed]
+ */
+function add_rel_preload($html, $handle, $href, $media) {
+    
+    if (is_admin())
+        return $html;
+
+     $html = <<<EOT
+<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
+EOT;
+    return $html;
+}
+add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
+
+
+
 /** 
  * Disabilito aggiornamenti temi e plugin
  */
